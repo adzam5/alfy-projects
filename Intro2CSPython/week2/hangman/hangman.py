1fd3f9c9062326601d89e9cb2ef26e7dfa40c5ed
j <j@3th0.net>
2022-11-15 23:48:23 -0500

    re-wrote code eliminated unnecessary variables fixed filter_words_list func to pass test re-wrote pattern_check func to provide better suggestions simplified main func

--- Intro2CSPython/week2/hangman/hangman.py
+++ Intro2CSPython/week2/hangman/hangman.py
@@ -9,10 +9,7 @@ if sys.stdout.isatty():
 else:
 	cWarn, cWin, cBold, cEnd = '', '', '', ''
 
-games_count = 0
-points = 0
-play = 1
-
+games_count = 1
 
 def update_word_pattern(word, pattern, letter):
 	for i in range(len(word)):
@@ -28,8 +25,8 @@ def filter_words_list(words, pattern, wrong_guess_lst):
 			if wrong_check(word, wrong_guess_lst) and pattern_check(pattern, word):
 				matches.append(word)
 	matched = len(matches)
-	sub = 0, matched//HINT_LENGTH, 2*matched//HINT_LENGTH
 	if matched > HINT_LENGTH:
+		sub = [0, matched//HINT_LENGTH, 2*matched//HINT_LENGTH, (HINT_LENGTH - 1)*matched//HINT_LENGTH]
 		sub_matches = []
 		for i in sub:
 			sub_matches.append(matches[i])
@@ -46,33 +43,25 @@ def wrong_check(word, wrong_guess_lst):
 
 
 def pattern_check(pattern, word):
-	for letter in pattern:
-		if letter != '_':
-			if letter not in word:
+	matches = 0
+	for pos, letter in enumerate(word):
+		if letter == pattern[pos]:
+			matches += 1
+			if word.count(letter) != pattern.count(letter):
 				return False
-	pairs = list(zip(pattern, word))
-	same = 0
-	for i in pairs:
-		if i[0] != '_' and i[0] == i[1]:
-			same += 1
-	us = pattern.count('_')
-	return same == (len(pattern) - us)
+	return True if matches == len(word) - pattern.count('_') else False
 
 
 def run_single_game(list_words, score):
 	global games_count
-	global points
-	global play
 	msg = f'{cBold}Welcome to Hangman!{cEnd}'
-	points = score
 	wrong_guess_lst = []
 	word = get_random_word(list_words)
 	pattern = '_' * len(word)
-	games_count += 1
 
-	print(f'word: {word}')  # Debugging
-	while word != pattern and points > 0:
-		display_state(pattern, wrong_guess_lst, points, msg)
+	# print(f'word: {word}')  # Debugging
+	while word != pattern and score > 0:
+		display_state(pattern, wrong_guess_lst, score, msg)
 		msg = ''
 		user_input = get_input()
 		guess = user_input[1]
@@ -81,43 +70,51 @@ def run_single_game(list_words, score):
 			if len(guess) > 1 or guess.isupper() or guess.isdigit() or not guess.isalpha():
 				msg = f'{cWarn}Input incorrect.  Please enter a lowercase letter.{cEnd}'
 			elif guess in word and guess not in pattern:
-				points -= 1
+				score -= 1
 				n = word.count(guess)
 			elif guess in wrong_guess_lst or guess in pattern:
 				msg = f"{cWarn}'{guess}' already guessed.  Please try again.{cEnd}"
 			elif guess not in wrong_guess_lst:
-				points -=1
+				score -=1
 				wrong_guess_lst.append(guess)
 		elif user_input[0] == WORD:
 			if len(guess) != len(word) or guess.isupper() or guess.isdigit() or not guess.isalpha():
 				msg = f'{cWarn}Input incorrect.  Please try again.{cEnd}'
 			elif guess == word:
-				points -= 1
+				score -= 1
 				n = pattern.count('_')
 				pattern = guess
 			elif guess != word:
-				points -=1
+				score -=1
 		elif user_input[0] == HINT:
-			points -= 1
+			score -= 1
 			words = list_words
 			show_suggestions(filter_words_list(words, pattern, wrong_guess_lst))
-		points = points + (n*(n+1)//2)
+		score = score + (n*(n+1)//2)
 		pattern = update_word_pattern(word, pattern, guess)
 	if pattern == word:
+		s = 's' if games_count > 1 else ''
 		msg = f'{cWin}YOU WIN!{cEnd}'
-		display_state(pattern, wrong_guess_lst, points, msg)
-		msg = f'You have played {games_count} games and have {points} points'
-	if points <= 0:
-		games_count = 0
-		msg = f'{cBold}You lose!  Your winning streak has ended after {games_count} wins.{cEnd}\nThe word was {word}.'
-	play = play_again(msg)
+		pa_msg = f'You have played {games_count} game{s} and have {score} points. Play again?'
+	else:
+		msg = f'The word was: {word}'
+		pa_msg = f'{cBold}You lose!  You won {games_count -1} out of {games_count} games.  Play again?{cEnd}'
+	display_state(pattern, wrong_guess_lst, score, msg)
+	games_count += 1
+	return score, pa_msg
 
 
 def main():
+	global games_count
 	list_words = load_words()
-	while play:
-		score = POINTS_INITIAL if games_count == 0 else points
-		run_single_game(list_words, score)
+	score = POINTS_INITIAL
+	score, msg = run_single_game(list_words, score)
+
+	while play_again(msg):
+		if score <= 0:
+			score = POINTS_INITIAL
+			games_count = 1
+		score, msg = run_single_game(list_words, score)
 
 
 if __name__ == "__main__":
